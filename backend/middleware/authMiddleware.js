79ef936d08bb2507d3f17696b609be5d34c1cc39
Tulsi-gohil <gohiltulsi451@gmail.com>
2025-12-19 12:34:38 +0530

    third commit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('./models/User');
+const User = require('../models/User');
 
 const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization || '';
